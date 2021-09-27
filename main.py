9900424686dc2b53fc75832edd1fe171b0e0a261
IrelandTom <3thistle1@gmail.com>
2021-09-27 13:45:30 +0100

    test

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 
 pygame.init()
 white = (255, 255, 255)
@@ -7,17 +8,21 @@ black = (0, 0, 0)
 
 class Screen:
     def __init__(self, width, height):
-        width1 = width
-        height1 = height
-        self.window = pygame.display.set_mode((width1, height1))
+        scr_width = width
+        scr_height = height
+        self.window = pygame.display.set_mode((scr_width, scr_height))
 
     def draw_win(self):
         self.window.fill(white)
-        pygame.display.update()
 
 
-class Thing:
-    pass
+class Thing(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("assets/man.png")
+        self.rect = self.image.get_rect()
+        self.thing_img = pygame.transform.scale(self.image, (55, 40))
+        self.rect.center = [pos_x, pos_y]
 
 
 def main():
@@ -25,13 +30,21 @@ def main():
     clock = pygame.time.Clock()
     screen = Screen(1360, 700)
 
-    run = True
-    while run:
+    # man test
+    man = Thing(100, 100)
+    man_group = pygame.sprite.Group()
+    man_group.add(man)
+
+    while True:
         clock.tick(50)
+        man_group.draw(screen.window)
         screen.draw_win()
+        pygame.display.update()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run = False
+                pygame.quit()
+                sys.exit()
 
 
 if __name__ == "__main__":
