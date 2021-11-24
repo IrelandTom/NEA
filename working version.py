978222dfe20d146551305e1938a6f95e4f6c369a
IrelandTom <3thistle1@gmail.com>
2021-11-24 15:50:39 +0000

    added better animations and also got animations working which is nice :DDDDDDDDD

--- working version.py
+++ working version.py
@@ -12,13 +12,12 @@ red = (255, 0, 0)
 steel_blue = (27, 51, 71)
 
 
+
 class Screen:
     def __init__(self, width, height):
         self.window_size = (width, height)
         self.screen = pygame.display.set_mode(self.window_size)
         self.display = pygame.Surface((width // 2, height // 2))
-        self.cheese = "123"
-
 
     def scale(self):
         new_surface = pygame.transform.scale(self.display, self.window_size)
@@ -59,25 +58,22 @@ class Map:
 
 
 class Player(pygame.sprite.Sprite):  # sprite class
-    def __init__(self, pos_x, pos_y, image_default, image_right, image_left, image_walking_left1, image_walking_left2,
-                 image_walking_right1, image_walking_right2, key_left, key_right, key_down, key_up):
+    def __init__(self, pos_x, pos_y, player_images, walking_images_left, walking_images_right, attack_images, key_left, key_right, key_down,
+                 key_up, key_attack):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(image_default)
-        self.image_right = pygame.image.load(image_right)
-        self.image_left = pygame.image.load(image_left)
-        self.image_walking_left1 = pygame.image.load(image_walking_left1)
-        self.image_walking_left2 = pygame.image.load(image_walking_left2)
-        self.image_walking_right1 = pygame.image.load(image_walking_right1)
-        self.image_walking_right2 = pygame.image.load(image_walking_right2)
-        self.image_right.set_colorkey(white)
-        self.image_left.set_colorkey(white)
-        self.image.set_colorkey(white)
+        self.image = player_images[0]
+        self.images_list = player_images
+        self.walking_images_left = walking_images_left
+        self.walking_images_right = walking_images_right
+        self.images_attack = attack_images
         self.rect = self.image.get_rect()
         self.rect.center = [pos_x, pos_y]
         self.left = key_left
         self.right = key_right
         self.down = key_down
         self.up = key_up
+        self.attack_button = key_attack
+        self.attacking = False
         self.moving_right = False
         self.moving_left = False
         self.moving_up = True
@@ -87,8 +83,20 @@ class Player(pygame.sprite.Sprite):  # sprite class
         self.x_velocity = 0
         self.falling = False
         self.jumping = False
+        self.animation_counter = 0
+        self.frame_counter = 0
         self.player_orientation = {"left": False, "right": False, "up": False, "down": False, "default": True}
         self.collision_types = {"top": False, "bottom": False, "right": False, "left": False}
+
+    def colour_key(self):
+        for item in self.walking_images_left:
+            self.images_list.append(item)
+        for item in self.walking_images_right:
+            self.images_list.append(item)
+        for item in self.images_attack:
+            self.images_list.append(item)
+        for image in self.images_list:
+            image.set_colorkey(white)
 
     def collision_test(self, tiles):
         # takes list of all tile rects then checks if they have collided with the players
@@ -165,13 +173,17 @@ class Player(pygame.sprite.Sprite):  # sprite class
         if event.type == KEYDOWN:
             if event.key == self.left:
                 self.moving_left = True
+                self.moving_right = False
                 self.x_velocity = -2
                 self.player_orientation["default"] = False
+                self.player_orientation["right"] = False
                 self.player_orientation["left"] = True
             if event.key == self.right:
                 self.moving_right = True
+                self.moving_left = False
                 self.x_velocity = 2
                 self.player_orientation["default"] = False
+                self.player_orientation["left"] = False
                 self.player_orientation["right"] = True
             if event.key == self.up:
                 self.player_orientation["default"] = False
@@ -183,46 +195,64 @@ class Player(pygame.sprite.Sprite):  # sprite class
             if event.key == self.down:
                 self.player_orientation["default"] = False
                 self.player_orientation["down"] = True
+            if event.key == self.attack_button:
+                self.attacking = True
+
         # can un-toggle the stuff from key downs or bind to new toggles
         if event.type == KEYUP:
             if event.key == self.left:
                 self.x_velocity = 0
                 self.moving_left = False
-                self.player_orientation["left"] = False
                 self.player_orientation["default"] = True
             if event.key == self.right:
                 self.x_velocity = 0
                 self.moving_right = False
-                self.player_orientation["right"] = False
                 self.player_orientation["default"] = True
-            if event.key == self.down:
+            if event.key == self.up:
                 self.player_orientation["up"] = False
                 self.player_orientation["default"] = True
             if event.key == self.down:
                 self.player_orientation["down"] = False
                 self.player_orientation["default"] = True
+            if event.key == self.attack_button:
+                self.attacking = False
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
     def player_animations(self):
         if self.player_orientation["left"]:
-            self.image = self.image_left
-        if self.player_orientation["right"]:
-            self.image = self.image_right
-        counter = 0
-        while self.moving_left:
-            counter += 1
-            if counter % 2 == 0:
-                self.image = self.image_walking_left1
-            if counter % 2 != 0:
-                self.image = self.image_walking_left2
+            self.frame_counter += 1
+            if self.moving_left:
+                if self.frame_counter % 5 == 0:
+                    self.animation_counter = (self.animation_counter + 1) % len(self.walking_images_left)
+                    self.image = self.walking_images_left[self.animation_counter]
+            if not self.moving_left:
+                self.image = self.images_list[1]
+            if self.attacking:
+                self.image = self.images_attack[1]
+            if not self.attacking:
+                pass
 
+        if self.player_orientation["right"]:
+            self.frame_counter += 1
+            if self.moving_right:
+                if self.frame_counter % 5 == 0:
+                    self.animation_counter = (self.animation_counter + 1) % len(self.walking_images_right)
+                    self.image = self.walking_images_right[self.animation_counter]
+            if not self.moving_right:
+                self.image = self.images_list[0]
+            if self.attacking:
+                self.image = self.images_attack[0]
+
+    def player_attack(self):
+        pass
 
 
 def main():
 
     # create objects
+    fps = 60
     clock = pygame.time.Clock()
     screen = Screen(1377, 705)
     screen_rect = screen.display.get_rect()
@@ -230,15 +260,54 @@ def main():
     map = Map("assets/map.txt")
     game_map = map.load_map()
     # players
-    player_1 = Player(200, 100, "assets/frog_p1_right.png", "assets/frog_p1_right.png", "assets/frog_p1_left.png", "assets/frog_p1_left_walk1.png", "assets/frog_p1_left_walk2.png", "assets/frog_p1_right_walk1.png", "assets/frog_p1_right_walk2.png", pygame.K_a, pygame.K_d, pygame.K_s, pygame.K_w)
-    player_2 = Player(250, 100, "assets/frog_p2_left.png", "assets/frog_p2_right.png", "assets/frog_p2_left.png", "assets/frog_p1_left_walk1.png", "assets/frog_p1_left_walk2.png", "assets/frog_p1_right_walk1.png", "assets/frog_p1_right_walk2.png", pygame.K_LEFT, pygame.K_RIGHT, pygame.K_DOWN, pygame.K_UP)
+    # player images lists
+    player_1_images = [pygame.image.load("assets/frog_p1_right.png"), pygame.image.load("assets/frog_p1_left.png")]
+    player_1_walking_sequence_left = [pygame.image.load("assets/frog_p1_left_walk1.0.png"),
+                                      pygame.image.load("assets/frog_p1_left_walk1.25.png"),
+                                      pygame.image.load("assets/frog_p1_left_walk1.50.png"),
+                                      pygame.image.load("assets/frog_p1_left_walk1.75.png"),
+                                      pygame.image.load("assets/frog_p1_left_walk2.0.png"),
+                                      pygame.image.load("assets/frog_p1_left_walk2.25.png"),
+                                      pygame.image.load("assets/frog_p1_left_walk2.50.png"),
+                                      pygame.image.load("assets/frog_p1_left_walk2.75.png"),
+                                      pygame.image.load("assets/frog_p1_left_walk2.76.png"),
+                                      pygame.image.load("assets/frog_p1_left_walk2.78.png")]
+
+    player_1_walking_sequence_right = [pygame.image.load("assets/frog_p1_right_walk1.0.png"),
+                                       pygame.image.load("assets/frog_p1_right_walk1.25.png"),
+                                       pygame.image.load("assets/frog_p1_right_walk1.50.png"),
+                                       pygame.image.load("assets/frog_p1_right_walk1.75.png"),
+                                       pygame.image.load("assets/frog_p1_right_walk2.0.png"),
+                                       pygame.image.load("assets/frog_p1_right_walk2.25.png"),
+                                       pygame.image.load("assets/frog_p1_right_walk2.50.png"),
+                                       pygame.image.load("assets/frog_p1_right_walk2.75.png"),
+                                       pygame.image.load("assets/frog_p1_right_walk2.76.png"),
+                                       pygame.image.load("assets/frog_p1_right_walk2.78.png")]
+    player_1_attack_sequence = [pygame.image.load("assets/frog_p1_attack_right.png"),
+                                pygame.image.load("assets/frog_p1_attack_left.png")]
+
+    player_2_images = [pygame.image.load("assets/frog_p2_right.png"), pygame.image.load("assets/frog_p2_left.png")]
+
+    player_2_walking_sequence = [pygame.image.load("assets/frog_p2_left_walk1.png"),
+                                 pygame.image.load("assets/frog_p2_left_walk2.png"),
+                                 pygame.image.load("assets/frog_p2_right_walk1.png"),
+                                 pygame.image.load("assets/frog_p2_right_walk2.png")]
+    player_2_attack_sequence = [pygame.image.load("assets/frog_p2_attack_right.png"),
+                                pygame.image.load("assets/frog_p2_attack_left.png") ]
+
+    player_1 = Player(200, 100, player_1_images, player_1_walking_sequence_left, player_1_walking_sequence_right, player_1_attack_sequence, pygame.K_a,
+                      pygame.K_d, pygame.K_s, pygame.K_w, pygame.K_LSHIFT)
+    player_2 = Player(250, 100, player_2_images, player_2_walking_sequence, player_2_walking_sequence, player_2_attack_sequence, pygame.K_LEFT,
+                      pygame.K_RIGHT, pygame.K_DOWN, pygame.K_UP, pygame.K_RSHIFT)
+    player_1.colour_key()
+    player_2.colour_key()
     player_group = pygame.sprite.Group()
     player_group.add(player_1, player_2)
 
     while True:
 
 
-        clock.tick(60)
+        clock.tick(fps)
         map.draw_map(screen)
 
         player_group.draw(screen.display)
